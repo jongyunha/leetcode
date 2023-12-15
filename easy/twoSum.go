84adf42f0dc6edfc3d4084f40fd5b24f14538f5a
00neiman00 <jongyun@seoulrobotics.org>
2023-12-15 20:00:07 +0900

    feat: easy two sum

---
+++ easy/twoSum.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for idx, num := range nums {
+		m[num] = idx
+	}
+
+	for idx, num := range nums {
+		if compareIdx, ok := m[target-num]; ok && compareIdx != idx {
+			return []int{idx, compareIdx}
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	nums := []int{2, 7, 5, 9, 4}
+	result := twoSum(nums, 9)
+	fmt.Println(result)
+}
