13aebf4dc29a9fa0faec4e5d27f35e7ca97e0d2d
jongyunha <jongyun@seoulrobotics.org>
2024-09-08 12:27:25 +0900

    leetcode: [hard] regular expression matching

--- hard/regular_expression_matching.go
+++ hard/regular_expression_matching.go
@@ -1,6 +1,6 @@
-package main
+package hard
 
-func isMatch(s string, p string) bool {
+func isMatch2(s string, p string) bool {
 	m, n := len(s), len(p)
 
 	dp := make([][]bool, m+1)
@@ -12,16 +12,16 @@ func isMatch(s string, p string) bool {
 
 	for j := 1; j <= n; j++ {
 		if p[j-1] == '*' {
-			dp[0][j] = dp[0][j-1]
+			dp[0][j] = dp[0][j-2]
 		}
 	}
 
 	for i := 1; i <= m; i++ {
 		for j := 1; j <= n; j++ {
-			if p[j-1] == s[i-1] || p[j-1] == '?' {
+			if p[j-1] == s[i-1] || p[j-1] == '.' {
 				dp[i][j] = dp[i-1][j-1]
 			} else if p[j-1] == '*' {
-				dp[i][j] = dp[i-1][j] || dp[i][j-1]
+				dp[i][j] = dp[i][j-2] || (dp[i-1][j] && (s[i-1] == p[j-2] || p[j-2] == '.'))
 			}
 		}
 	}
--- hard/regular_expression_matching_test.go
+++ hard/regular_expression_matching_test.go
@@ -1,8 +1,8 @@
-package main
+package hard
 
 import "testing"
 
-func Test_wildcard_matching(t *testing.T) {
+func Test_regular_expression_matching(t *testing.T) {
 	tests := []struct {
 		s    string
 		p    string
@@ -10,16 +10,14 @@ func Test_wildcard_matching(t *testing.T) {
 	}{
 		{"aa", "a", false},
 		{"aa", "a*", true},
-		{"cb", "?a", false},
-		{"adceb", "*a*b", true},
-		{"acdcb", "a*c?b", false},
-		{"", "******", true},
-		{"abc", "a**", true},
-		{"abc", "a**c", true},
+		{"cb", ".a", false},
+		{"adceb", ".*a*b", true},
+		{"acdcb", "a.*c.b", false},
+		{"aab", "c*a*b", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.s+" "+tt.p, func(t *testing.T) {
-			if got := isMatch(tt.s, tt.p); got != tt.want {
+			if got := isMatch2(tt.s, tt.p); got != tt.want {
 				t.Errorf("isMatch() = %v, want %v", got, tt.want)
 			}
 		})
